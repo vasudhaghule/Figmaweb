26841b95a47dd1f30f3875569f1a7ca2a8ded4f2
Vasudha Ghule <133812225+vasudhaghule@users.noreply.github.com>
2024-11-18 17:32:48 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 import React from "react";
 import Wrapper from "@/components/shared/Wrapper";
 import Link from "next/link";
-import logo from "/public/logo.png"
-
 
 const SvgIcon = () => <svg width="27" height="15"></svg>;
 
@@ -12,9 +10,10 @@ const Header = () => {
     <header className="">
       <Wrapper>
         <div className="flex justify-between items-center py-5">
-        <Link className="hover:text-teal-700 duration-300" href={"/"}>
+          {/* Correct way to reference an image from the public folder */}
+          <Link className="hover:text-teal-700 duration-300" href={"/"}>
             <div>
-              <Image src={logo} alt="Panaverse dao" />
+              <Image src="/logo.png" alt="Panaverse dao" width={100} height={50} />
             </div>
           </Link>
 
@@ -24,7 +23,11 @@ const Header = () => {
             <Link href={""} className="flex items-center hover:text-white-700 duration-300">Use Cases <SvgIcon /></Link>
             <Link href={""} className="flex items-center hover:text-white-700 duration-300">Pricing<SvgIcon /></Link>
             <Link href={""} className="flex items-center hover:text-white-700 duration-300">Blog <SvgIcon /></Link>
-            <Link href={""}><button className="px-9 py-3 border border-black text-black font-medium rounded-md  "> Request a quote </button></Link>
+            <Link href={""}>
+              <button className="px-9 py-3 border border-black text-black font-medium rounded-md">
+                Request a quote
+              </button>
+            </Link>
           </nav>
         </div>
       </Wrapper>
